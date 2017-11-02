1b6b28b2e975a1ec34227d894bfe548843cb0f2b
Your Name <you@example.com>
2017-11-02 21:16:07 +0200

    1

--- train.py
+++ train.py
@@ -20,9 +20,9 @@ def load_images_from_folder(folder):
         file_name = int(os.path.splitext(filename)[0])
 
 
-        if file_name <= 5000:
+        if file_name <= 10000:
             file_name = 1
-        elif 11520 < file_name <= 16520:
+        elif 11520 < file_name <= 21520:
             file_name = 2
 	else:
 	    continue
